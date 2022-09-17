bde95abd13ced3475da6a4aaf6414f11552cfb17
Fasteron <bohdanburkush@gmail.com>
2022-09-17 11:38:13 +0300

    .

---
+++ js.js
@@ -0,0 +1,28 @@
+
+// Перше
+
+//let before = 2000;
+//let now = 2022;
+//let age = now - before;
+
+//alert("Your age " + age);
+
+
+// Друге
+
+let a = +prompt("Введи якесь число");
+let b = a + 1;
+let c = b + 1;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
+
+
+
+
+
+//let a = Math.floor(Math.random() * 10);
